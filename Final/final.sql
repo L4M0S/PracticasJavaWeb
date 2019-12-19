17adb2907fbc2429fce1f10518fa0f154e626d6b
L4M0S <13170563+L4M0S@users.noreply.github.com>
2019-12-19 07:53:20 -0600

    Create final.sql

---
+++ Final/final.sql
@@ -0,0 +1,104 @@
+CREATE TABLE `cliente` (
+  `IdCliente` int(11) UNSIGNED NOT NULL,
+  `Dni` varchar(8) DEFAULT NULL,
+  `Nombres` varchar(244) DEFAULT NULL,
+  `Direccion` varchar(244) DEFAULT NULL,
+  `Estado` varchar(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `cliente` (`IdCliente`, `Dni`, `Nombres`, `Direccion`, `Estado`) VALUES
+(17, '2', 'Juan Guerrero Solis', 'Los Alamos', '1'),
+(18, '1', 'Maria Rosas Villanueva', 'Los Laureles 234', '1'),
+(19, '3', 'Andres de Santa Cruz', 'Av. La Frontera 347', '1'),
+(20, '4', 'Andres Mendoza', 'Chosica, Lurigancho', '1');
+
+CREATE TABLE `detalle_ventas` (
+  `IdDetalleVentas` int(11) UNSIGNED NOT NULL,
+  `IdVentas` int(11) UNSIGNED NOT NULL,
+  `IdProducto` int(11) UNSIGNED NOT NULL,
+  `Cantidad` int(11) UNSIGNED DEFAULT NULL,
+  `PrecioVenta` double DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `empleado` (
+  `IdEmpleado` int(10) UNSIGNED NOT NULL,
+  `Dni` varchar(8) NOT NULL,
+  `Nombres` varchar(255) DEFAULT NULL,
+  `Telefono` varchar(9) DEFAULT NULL,
+  `Estado` varchar(1) DEFAULT NULL,
+  `User` varchar(8) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `empleado` (`IdEmpleado`, `Dni`, `Nombres`, `Telefono`, `Estado`, `User`) VALUES
+(1, '123', 'Pedro Hernandez', '988252459', '1', 'emp01'),
+(2, '123', 'Roman Riquelme', '988252459', '1', 'Jo46'),
+(3, '123', 'Palermo Suarez', '453536458', '1', 'Em22');
+
+
+CREATE TABLE `producto` (
+  `IdProducto` int(11) UNSIGNED NOT NULL,
+  `Nombres` varchar(244) DEFAULT NULL,
+  `Precio` double DEFAULT NULL,
+  `Stock` int(11) UNSIGNED DEFAULT NULL,
+  `Estado` varchar(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `producto` (`IdProducto`, `Nombres`, `Precio`, `Stock`, `Estado`) VALUES
+(1, 'Teclado Logitech 345 Editado', 150, 99, '1'),
+(2, 'Mouse Logitech 567', 20, 98, '1'),
+(3, 'Laptop Lenovo Ideapad 520', 800, 100, '1'),
+(4, 'HeadPhones Sony M333', 500, 98, '1'),
+(7, 'Producto Nuevo w', 22, 35, '1');
+
+CREATE TABLE `ventas` (
+  `IdVentas` int(11) UNSIGNED NOT NULL,
+  `IdCliente` int(11) UNSIGNED NOT NULL,
+  `IdEmpleado` int(10) UNSIGNED NOT NULL,
+  `NumeroSerie` varchar(244) DEFAULT NULL,
+  `FechaVentas` date DEFAULT NULL,
+  `Monto` double DEFAULT NULL,
+  `Estado` varchar(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`IdCliente`);
+ALTER TABLE `detalle_ventas`
+  ADD PRIMARY KEY (`IdDetalleVentas`,`IdVentas`,`IdProducto`),
+  ADD KEY `Ventas_has_Producto_FKIndex1` (`IdVentas`),
+  ADD KEY `Ventas_has_Producto_FKIndex2` (`IdProducto`);
+
+ALTER TABLE `empleado`
+  ADD PRIMARY KEY (`IdEmpleado`);
+
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`IdProducto`);
+
+ALTER TABLE `ventas`
+  ADD PRIMARY KEY (`IdVentas`),
+  ADD KEY `Ventas_FKIndex1` (`IdEmpleado`),
+  ADD KEY `Ventas_FKIndex2` (`IdCliente`);
+
+
+ALTER TABLE `cliente`
+  MODIFY `IdCliente` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+ALTER TABLE `detalle_ventas`
+  MODIFY `IdDetalleVentas` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=155;
+
+ALTER TABLE `empleado`
+  MODIFY `IdEmpleado` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+ALTER TABLE `producto`
+  MODIFY `IdProducto` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+ALTER TABLE `ventas`
+  MODIFY `IdVentas` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=94;
+
+ALTER TABLE `detalle_ventas`
+  ADD CONSTRAINT `detalle_ventas_ibfk_1` FOREIGN KEY (`IdVentas`) REFERENCES `ventas` (`IdVentas`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `detalle_ventas_ibfk_2` FOREIGN KEY (`IdProducto`) REFERENCES `producto` (`IdProducto`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+ALTER TABLE `ventas`
+  ADD CONSTRAINT `ventas_ibfk_1` FOREIGN KEY (`IdEmpleado`) REFERENCES `empleado` (`IdEmpleado`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `ventas_ibfk_2` FOREIGN KEY (`IdCliente`) REFERENCES `cliente` (`IdCliente`) ON DELETE NO ACTION ON UPDATE NO ACTION;
